7c2001da6e5edfa1252b8e938b3e5cb4afe7311f
amichaikafka <amichaikp@gmail.com>
2021-12-01 19:52:08 +0200

    second

--- app/src/main/java/com/example/myapplication2/OpenScreen.java
+++ app/src/main/java/com/example/myapplication2/OpenScreen.java
@@ -1,8 +1,11 @@
 package com.example.myapplication2;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -26,6 +29,9 @@ public class OpenScreen extends AppCompatActivity {
 //        txtName=(EditText)findViewById(R.id.txtName);
 //        name=txtName.getText().toString();
 //        System.out.println("____________"+name);
+        ActionBar actionBar=getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setTitle(getResources().getString(R.string.app_name));
         txtPass=(EditText)findViewById(R.id.TextPassword);
         String pass=txtPass.getText().toString();
 
@@ -49,15 +55,24 @@ public class OpenScreen extends AppCompatActivity {
     }
 
     public void onChangeLan(View view) {
+        final String[] lang={"English","עברית"};
         flag=!flag;
-        if(flag) {
-       setLocal("he");
-       recreate();
-        }else{
-            setLocal("");
-            recreate();
+        System.out.println("asfffffffffffffffffffffffffffffffffffffff");
+        AlertDialog.Builder myBuilder=new AlertDialog.Builder(OpenScreen.this);
+        myBuilder.setSingleChoiceItems(lang, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(which==0) {
+                    setLocal("iw");
+                    recreate();
+                }if(which==1){
+                    setLocal("iw");
+                    recreate();
 
-        }
+                }
+            }
+        });
+
 
     }
     public void load(){

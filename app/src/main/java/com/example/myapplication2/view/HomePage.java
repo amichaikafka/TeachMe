2eb2b61323167c8b056c434bcd92cf7a7fb74ebe
amichaikafka <amichaikp@gmail.com>
2021-12-14 16:00:43 +0200

    register and read name from firebase

--- app/src/main/java/com/example/myapplication2/view/HomePage.java
+++ app/src/main/java/com/example/myapplication2/view/HomePage.java
@@ -8,15 +8,51 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myapplication2.R;
+import com.example.myapplication2.api.StudentProfile;
+import com.example.myapplication2.api.UserProfile;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class HomePage extends AppCompatActivity {
+    private FirebaseUser mAuth;
+    FirebaseDatabase database;
+    DatabaseReference myRef=null;
+    TextView hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_home_page);
+        mAuth = FirebaseAuth.getInstance().getCurrentUser();
+        hello=(TextView)findViewById(R.id.name_filled_search);
+        database= FirebaseDatabase.getInstance("https://teachme-c8637-default-rtdb.firebaseio.com/");
+
+        DatabaseReference myRef = database.getReference("Students/"+mAuth.getUid());
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                StudentProfile userProfile=snapshot.getValue(StudentProfile.class);
+                hello.setText(userProfile.getFirstName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(HomePage.this, "error reading from firebase", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
     @Override

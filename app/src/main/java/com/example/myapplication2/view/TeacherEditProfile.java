2c245209e7093eb3d3c23ff688ffd30b4caa1036
itai <itai.lash@gmail.com>
2021-12-18 19:09:43 +0200

    change names

---
+++ app/src/main/java/com/example/myapplication2/view/TeacherEditProfile.java
@@ -0,0 +1,14 @@
+package com.example.myapplication2.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeacherEditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_edit_profile);
+    }
+}

2d84dcd26395c333b15cc53dfb034e6608dcf595
liavweiss <liavweiss@gmail.com>
2021-12-22 22:13:21 +0200

    fixed the menu

--- app/src/main/java/com/example/myapplication2/view/TeacherEditProfile.java
+++ app/src/main/java/com/example/myapplication2/view/TeacherEditProfile.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -83,6 +84,14 @@ public class TeacherEditProfile extends AppCompatActivity {
         Button updateBtn = (Button)view;
         updateTeacher();
         myRef.setValue(currTeacher);
+    }
+
+
+    //** menu **//
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity, menu);
+        return true;
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
